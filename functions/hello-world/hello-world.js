7c4892b2bd893db7a395746e89e35234904aa2fe
Hiren <hiren@freshbits.in>
2020-05-30 17:37:11 +0530

    Get file path log

--- functions/hello-world/hello-world.js
+++ functions/hello-world/hello-world.js
@@ -4,16 +4,23 @@ const util = require('util');
 
 exports.handler = async (event, context, callback) => {
   try {
-    const getNewslettersFrom = util.promisify(fileSystem.readFile).bind(fileSystem);
-
+    console.log(__dirname);
+    // const getNewslettersFrom = util.promisify(fileSystem.readFile).bind(fileSystem);
     return {
       statusCode: 200,
       body: JSON.stringify({
-        newsletters: await getNewslettersFrom('./database/newsletters.json').then((requestResults) => {
-          return JSON.parse(requestResults);
-        })
+        newsletters: __dirname
       }),
     };
+
+    // return {
+    //   statusCode: 200,
+    //   body: JSON.stringify({
+    //     newsletters: await getNewslettersFrom('./database/newsletters.json').then((requestResults) => {
+    //       return JSON.parse(requestResults);
+    //     })
+    //   }),
+    // };
   } catch (err) {
     return { statusCode: 500, body: err.toString() }
   }
